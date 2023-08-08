f5ae3dce066896002d3dfeaad34643c885414e5c
Mohamad Hesari <mdhesari99@gmail.com>
2023-08-08 16:04:52 +0330

    fix install command

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -25,13 +25,13 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        if (! config('assistant.openapi_api_key')) {
+        if (! config('laravel-assistant.openapi_api_key')) {
             $this->error('Please add OPENAI_API_KEY to your .env variables.');
             return 1;
         };
 
         if ($this->confirm(question: 'would you install the dependencies? (if not do it manually later)', default: false)) {
-            exec('composer install mdhesari/api-response mdhesari/laravel-query-filter');
+            exec('composer require mdhesari/api-response mdhesari/laravel-query-filter');
         }
 
         return 0;
